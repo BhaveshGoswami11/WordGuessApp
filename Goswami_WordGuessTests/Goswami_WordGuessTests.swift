ae15410d05f90eb2ba97db91661ea85a4702bf7f
Bhavesh Goswami <goswamjbhavesh@gmail.com>
2025-10-27 09:40:09 -0500

    Create Goswami_WordGuessTests.swift

---
+++ Goswami_WordGuessTests/Goswami_WordGuessTests.swift
@@ -0,0 +1,17 @@
+//
+//  Goswami_WordGuessTests.swift
+//  Goswami_WordGuessTests
+//
+//  Created by Bhavesh on 10/10/25.
+//
+
+import Testing
+@testable import Goswami_WordGuess
+
+struct Goswami_WordGuessTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
